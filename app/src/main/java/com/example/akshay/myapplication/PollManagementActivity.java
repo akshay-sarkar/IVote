7227c5fa948ddad9d8abdd15fb57c34c0f1d1fe2
Akshay Sarkar <akshay.sarkar.dbit@gmail.com>
2017-03-07 12:39:37 -0600

    Add Poll Screen
    Add Poll Screen

--- app/src/main/java/com/example/akshay/myapplication/PollManagementActivity.java
+++ app/src/main/java/com/example/akshay/myapplication/PollManagementActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.ListView;
